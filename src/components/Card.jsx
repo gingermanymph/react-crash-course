0f14bf2594789c0b522be4da5d242d6527ade3f5
gingermanymph <gingermanymph@gmail.com>
2024-11-16 16:29:08 +0200

    Before applying router

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+const Card = ({ children, bg = 'bg-gray-100' }) => {
+    return (
+        <div className={`${bg} p-6 rounded-lg shaddow-md`}>
+            {children}
+        </div>
+    )
+}
+
+export default Card
