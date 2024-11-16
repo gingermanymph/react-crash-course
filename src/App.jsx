b4b94faf52fbed46229c70bcf8028ead573db362
gingermanymph <gingermanymph@gmail.com>
2024-11-16 20:08:22 +0200

    Fiinish

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,71 @@
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<h1>hello</h1>}/>)
-);
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+import NotFoundPage from './pages/NotFoundPage';
+import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
+import EditJobPage from './pages/EditJobPage';
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  )
+  // Add new Job
+  const addJob = async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    });
+    return
+  }
+
+  // Update  Job
+  const updateJob = async (job) => {
+    console.log(job);
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(job)
+    });
+    return
+  }
+
+  // Delete Job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE'
+    });
+    const data = await res.json();
+    return
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage />} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route
+          path='/edit-job/:id'
+          element={<EditJobPage updateJobSubmit={updateJob} />}
+          loader={jobLoader}
+        />
+        <Route
+          path='/jobs/:id'
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
+        <Route path='*' element={<NotFoundPage />} />
+
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,21 +1,45 @@
-import Jobs from '../jobs.json';
+import { useState, useEffect } from 'react';
 import JobListing from './JobListing';
+import Spinier from './Spinner';
 
-const JobListings = () => {
+const JobListings = ({ isHome = false }) => {
 
-    const recentJobs = Jobs.slice(0,3);
+    const [jobs, setJobs] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fatchJobs = async () => {
+            const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
+            try {
+                const res = await fetch(apiUrl);
+                const data = await res.json();
+                setJobs(data);
+            } catch (error) {
+                console.log("Error fetching data", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fatchJobs();
+    }, [])
 
     return (
         <section className="bg-blue-50 px-4 py-10">
             <div className="container-xl lg:container m-auto">
                 <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-                    Browse Jobs
+                    {isHome ? 'Recent Jobs' : 'Browse Jobs'}
                 </h2>
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                    {recentJobs.map((job) => (
-                        <JobListing key={job.id} job={job} />
-                    ))}
-                </div>
+
+                {loading ? (
+                    <Spinier loading={loading} />
+                ) : (
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {jobs.map((job) => (
+                            <JobListing key={job.id} job={job} />
+                        ))}
+                    </div>
+                )}
+
             </div>
         </section>
     )
